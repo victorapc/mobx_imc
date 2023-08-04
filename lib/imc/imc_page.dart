f1ab9c97ca0bd0728249c514195172e5a6c15b1b
victor <victor.alexandre@atlantasistemas.com>
2023-08-04 17:35:43 -0300

    Widgets gauge range

---
+++ lib/imc/imc_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class ImcPage extends StatefulWidget {
+  const ImcPage({super.key});
+
+  @override
+  State<ImcPage> createState() => _ImcPageState();
+}
+
+class _ImcPageState extends State<ImcPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(''),
+      ),
+      body: Container(),
+    );
+  }
+}
